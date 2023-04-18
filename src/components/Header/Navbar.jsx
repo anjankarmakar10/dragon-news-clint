f3ab4f3a3d5b534f74373d0140cd53be57266e6d
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-04-18 18:34:06 +0600

    implement colapsoble navbar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { RxAvatar } from "react-icons/rx";
-import { FiMenu } from "react-icons/fi";
+import { FiMenu, FiX } from "react-icons/fi";
 import ButtonLogin from "../buttons/ButtonLogin";
 
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const handleMenuClicked = () => setActive((prev) => !prev);
+
   return (
     <div className="mt-5 mb-8 flex flex-col gap-4">
       <nav className=" flex items-center justify-between md:mb-10">
         <div className="cursor-pointer md:hidden">
-          <FiMenu size={40} />
+          {active ? (
+            <FiX onClick={handleMenuClicked} size={40} />
+          ) : (
+            <FiMenu onClick={handleMenuClicked} size={40} />
+          )}
         </div>
         <div></div>
         <ul className="text-lg text-[#706F6F] hidden md:flex items-center gap-4 ">
@@ -31,7 +39,11 @@ const Navbar = () => {
         </div>
       </nav>
 
-      <div className="hidden">
+      <div
+        className={`transition-all h-0 overflow-hidden opacity-0 ${
+          active && "h-fit opacity-100"
+        }`}
+      >
         <MobileNav />
       </div>
     </div>
@@ -40,8 +52,8 @@ const Navbar = () => {
 
 const MobileNav = () => {
   return (
-    <nav className=" flex justify-center">
-      <ul className="text-lg text-[#706F6F] flex items-center gap-4 ">
+    <nav className="bg-slate-400 p-2 rounded-md ">
+      <ul className="text-lg text-[#250101] flex items-center gap-4 justify-evenly">
         <li>
           <a href="#">Home</a>
         </li>
